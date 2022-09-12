f80d87b1c730498bdcb418c47c2eef6795d7f933
Lumbril <32036622+Lumbril@users.noreply.github.com>
2022-09-12 19:22:39 +0300

    fix models and added get method for purchase

--- core/api/models.py
+++ core/api/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=100, verbose_name='Названние')
     description = models.TextField(verbose_name='Описание')
-    price = models.FloatField(validators=[MinValueValidator(0)], verbose_name='Цена')
+    price = models.PositiveIntegerField(verbose_name='Цена')
 
     class Meta:
         db_table = 'items'
--- core/api/views.py
+++ core/api/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render, get_object_or_404
+import stripe
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 
 from api import models
 
+stripe.api_key = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+
 
 class PurchaseView(View):
     def get(self, request, *args, **kwargs):
-        pass
+        item = get_object_or_404(models.Item, id=kwargs['id'])
+
+        session = stripe.checkout.Session.create(
+            line_items=[{
+                'price_data': {
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': item.name,
+                        'description': item.description,
+                    },
+                    'unit_amount': item.price,
+                },
+                'quantity': 1,
+            }],
+            mode='payment',
+            success_url='https://example.com/success',
+            cancel_url='https://example.com/cancel',
+        )
+
+        return JsonResponse(data={'session_id': session.id})
 
 
 class ItemView(View):
@@ -16,7 +39,7 @@ class ItemView(View):
         context = {
             'name': item.name,
             'description': item.description,
-            'price': item.price
+            'price': item.price / 100
         }
 
         return render(request, 'index.html', context=context)
